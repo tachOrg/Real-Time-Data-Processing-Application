e7963e19f57ee5efbbf48362f02052852963872b
XrosHeart05 <lucampos2305@gmail.com>
2023-01-30 21:32:33 -0600

    remote lambdas and locale lambdas separated in different paths

--- src/local_lambdas/dynamo-db.js
+++ src/local_lambdas/dynamo-db.js
@@ -1,18 +1,19 @@
-import { DynamoDBClient, BatchWriteItemCommand } from '@aws-sdk/client-dynamodb';
+const { DynamoDBClient, BatchWriteItemCommand } = require('@aws-sdk/client-dynamodb');
+const { config } = require('dotenv');
 
-export const handler = function(event, context, callback) {
-};
+// Start up the .env config
+config();
 
 const dynamoDBClient = new DynamoDBClient({ region: process.env.AWS_REGION });
-const dynamoDBName = process.env.TABLE_NAME;
+const dynamoDBName = process.env.AWS_DYNAMO_DB_NAME;
 let commandInput = {
   RequestItems: {
     [dynamoDBName] : [ 
       {
         "PutRequest": {
           "Item": {
-            "Name": { "S": "Koethl" },
-            "StatusTime": { "S": "2023-01-30 21:30:00.000" }
+            "Name": { "S": "Bikora" },
+            "StatusTime": { "S": "2023-01-30 19:38:00.000" }
           }
         }
       }
@@ -21,7 +22,7 @@ let commandInput = {
 };
 const command = new BatchWriteItemCommand(commandInput);
 
-let insertIntoDB = async () => {
+insertIntoDB = async () => {
   try {
     await dynamoDBClient.send(command);
   } catch (error) { 
