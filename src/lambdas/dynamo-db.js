069b964ccbf0108797734fe17e58289e9d7f229a
XrosHeart05 <lucampos2305@gmail.com>
2023-02-01 22:24:16 -0600

    Reading stream and pushing to dynamo-db

--- src/lambdas/dynamo-db.js
+++ src/lambdas/dynamo-db.js
@@ -2,7 +2,7 @@ import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
 
 export const handler = function(event, context, callback) {
   console.log('Este es el event: ' + JSON.stringify(event));
-  const requestItems = buildRequestItems(event.Records);
+  const requestItems = buildRequestItems(event.Records[0]);
   console.log('Request items: ' + JSON.stringify(requestItems))
   const itemInput = buildRequests(requestItems);
   console.log('Item Input: ' + JSON.stringify(itemInput));
@@ -15,41 +15,24 @@ export const handler = function(event, context, callback) {
 };
 
 let buildRequestItems = (records) => {
-  return records.map((record) => {
-    const json = Buffer.from(record.kinesis.data, 'base64').toString('ascii');
-    const item = JSON.parse(json);
-    
-    return {
-      PutRequest: {
-        Item: item
-      }
+  const json = Buffer.from(records.kinesis.data, 'base64').toString('ascii');
+  const item = JSON.parse(json);
+  return {
+    PutRequest: {
+      Item: item
     }
-  });
+  }
 }
 
 let buildRequests = (requestItems) => {
-  let toReturn = {
+  return {
     "Name": {
-      S: ""
+      S: requestItems.PutRequest.Item.Name
     },
     "StatusTime": {
-      S: ""
+      S: requestItems.PutRequest.Item.StatusTime
     }
   };
-  console.log('Request items length: ' + requestItems.length);
-  while (requestItems.length > 0) {
-    const request = requestItems.splice(0, 25);
-    console.log('Req: ' + JSON.stringify(request));
-    let name = { "Name": { "S": "" } };
-    let time = { "StatusTime": { "S" : "" } };
-    let nt = Math.random(1, 100).toString().substring(5);
-    let req = request.map((r) => {
-      toReturn.Name.S = nt
-      toReturn.StatusTime.S = nt;
-    });
-  }
-  
-  return toReturn;
 }
 
 const dynamoDBClient = new DynamoDBClient({ region: process.env.AWS_REGION });
@@ -72,21 +55,9 @@ let commandInput = {
 };
 // const command = new BatchWriteItemCommand(commandInput);
 
-let insertIntoDB = async (command) => {
-  console.log('Ingresé al insertIntoDB, comando: '+ JSON.stringify(command));
+let insertIntoDB = async (inputFromMain) => {
   try {
-    let finalCommand = new PutItemCommand(command);
-    console.log('This is the final command: ' + JSON.stringify(finalCommand));
-    let fc = {
-      "Item": {
-            "Name": "Liarty",
-            "StatusTime": "2023-02-01 02:08:07.652"
-        },
-        "TableName": "UnicornSensorData"
-    };
-    // let finalCommand2 = new PutItemCommand(command);
-    // console.log('This is the final command 2: ' + JSON.stringify(finalCommand2));
-    // await dynamoDBClient.send(finalCommand2);
+    let finalCommand = new PutItemCommand(inputFromMain);
     await dynamoDBClient.send(finalCommand);
     console.log('Pasé el envío');
   } catch (error) { 
